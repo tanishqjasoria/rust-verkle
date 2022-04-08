1b808e325adfa65cffab89972650a11006d200f2
Manan Sharma <manan180796@gmail.com>
2022-04-08 22:58:17 +0530

    Cleanup

--- verkle-spec/src/hash.rs
+++ verkle-spec/src/hash.rs
@@ -1,14 +1,14 @@
-use verkle_trie::{committer::Committer,Fr,from_to_bytes::ToBytes};
-use ethereum_types::{H256};
 use crate::Hasher;
+use ethereum_types::H256;
+use verkle_trie::{committer::Committer, from_to_bytes::ToBytes, Fr};
 
 pub struct PedersenHasher;
-impl Hasher for PedersenHasher{
-    fn hash64(bytes64: [u8; 64]) -> H256{
+impl Hasher for PedersenHasher {
+    fn hash64(bytes64: [u8; 64]) -> H256 {
         use verkle_trie::committer::test::TestCommitter;
         // let hasher = PedersenHasher::new(TestCommitter);
         let chunks = crate::util::chunk64(bytes64);
-        let fr_data:Vec<_> = chunks.iter().map(|x| -> Fr {Fr::from(*x)}).collect();
+        let fr_data: Vec<_> = chunks.iter().map(|x| -> Fr { Fr::from(*x) }).collect();
         let bytes = TestCommitter.commit_lagrange(&fr_data[..]).to_bytes();
         return H256::from_slice(&bytes[..]);
         // hasher.hash(&chunks[..])
@@ -16,12 +16,12 @@ impl Hasher for PedersenHasher{
 }
 
 #[cfg(test)]
-mod test{
-    use crate::hash::{PedersenHasher,Hasher};
-    use hex::FromHex;
+mod test {
+    use crate::hash::{Hasher, PedersenHasher};
     use crate::H256;
+    use hex::FromHex;
     #[test]
-    fn hash_test(){
+    fn hash_test() {
         let tests = [
             (
                 <[u8;64]>::from_hex("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").expect("Decoding failed"),
@@ -36,8 +36,11 @@ mod test{
                 <[u8;32]>::from_hex("6fc5ac021ff2468685885ad7fdb31a0c58d1ee93254a58c9e9e0809187c53e71").expect("Decoding failed")
             )
         ];
-        for (input,output) in tests.iter(){
-            assert_eq!(PedersenHasher::hash64(*input),H256::from_slice(&output[..]));
+        for (input, output) in tests.iter() {
+            assert_eq!(
+                PedersenHasher::hash64(*input),
+                H256::from_slice(&output[..])
+            );
         }
     }
 }
